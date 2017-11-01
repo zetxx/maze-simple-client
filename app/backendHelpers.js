6b372260e7415bf01052d1a91be0be4bea9ea801
zetxx <zetxx@belogradcik.biz>
2017-11-01 23:08:24 +0200

    fix: price calc

--- app/backendHelpers.js
+++ app/backendHelpers.js
@@ -1,28 +1,38 @@
-module.exports.round = (number, precision) => {
+const round = (number, precision) => {
   var factor = Math.pow(10, precision)
   var tempNumber = number * factor
   var roundedTempNumber = Math.round(tempNumber)
   return roundedTempNumber / factor
 }
 
+const priceDiffCalc = (price, percentage, hardValue, simpleAdd) => {
+  return round(((price * ((percentage / 100) + 1)) + hardValue) - price, 2)
+}
+
+module.exports.round = round
+
+var simpleAdd = false
+
 module.exports.priceCalc = (priceRules) => {
   return (p) => {
+    var simpleAddArray = 0
+
     return priceRules.reduce((price, priceRule) => {
-      if (priceRule.rule === '>') {
-        if (price > priceRule.ruleValueFrom) {
-          return module.exports.round((price * ((priceRule.percentage / 100) + 1)) + priceRule.hardValue, 2)
+      if (
+        (priceRule.rule === '>' && price > priceRule.ruleValueFrom) ||
+        (priceRule.rule === '<' && price < priceRule.ruleValueFrom) ||
+        (priceRule.rule === 'between' && price >= priceRule.ruleValueFrom && price <= priceRule.ruleValueTo)
+      ) {
+        var priceDiff = priceDiffCalc(price, priceRule.percentage, priceRule.hardValue, simpleAdd)
+        if (!simpleAdd) {
+          price = price + priceDiff
+        } else {
+          simpleAddArray = simpleAddArray + priceDiff
         }
-      } else if (priceRule.rule === '<') {
-        if (price < priceRule.ruleValueFrom) {
-          return module.exports.round((price * ((priceRule.percentage / 100) + 1)) + priceRule.hardValue, 2)
-        }
-      } else if (priceRule.rule === 'between') {
-        if (price >= priceRule.ruleValueFrom && price <= priceRule.ruleValueTo) {
-          return module.exports.round((price * ((priceRule.percentage / 100) + 1)) + priceRule.hardValue, 2)
-        }
+        return price
       }
 
       return price
-    }, p)
+    }, p) + simpleAddArray
   }
 }
